84aab05bc27f0d68e365a4143f97c846f3380c5b
Rafael Almeida <leafaralmeida98@gmail.com>
2025-03-10 14:54:38 -0300

    Create dicionario-class.py

---
+++ dicionario-class/dicionario-class.py
@@ -0,0 +1,13 @@
+class Carro:
+
+    def __init__(self, marca: str, modelo: str, ano: int):
+
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+
+lista_carro = [
+    ("toyota", "corolla", "2000"),
+    ("alguma coisa", "alguma coisa", "2020")
+]
+print(lista_carro)
